e331bc95e1d47c83e27f589040ee4e99799b4b72
NICKMUGO <136487345+NICKMUGO@users.noreply.github.com>
2024-11-22 11:21:54 +0300

    changes

--- PHPMailer/PHPMailerAutoload.php
+++ PHPMailer/PHPMailerAutoload.php
@@ -42,8 +42,12 @@ if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
      * Fall back to traditional autoload for old PHP versions
      * @param string $classname The name of the class to load
      */
-    function __autoload($classname)
-    {
-        PHPMailerAutoload($classname);
-    }
+    spl_autoload_register(function ($class) {
+        // Make sure the path is correct for your project structure
+        $file = __DIR__ . '/' . $class . '.php';
+        if (file_exists($file)) {
+            require $file;
+        }
+    });
+    
 }
